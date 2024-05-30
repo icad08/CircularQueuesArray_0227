1970c1f0765dbb12dae92aae13a7caf4a8eaf28f
icad08 <irsyadrm08@gmail.com>
2024-05-30 14:35:59 +0700

    add class Queues with the member

--- queue.cpp
+++ queue.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 using namespace std;
 
-int main(){
+class Queues{
+private:
+    static const int max = 3;
+    int front, rear;
+    int queue_array[max];
+
+public: 
+    Queues(){
+        front = -1;
+        rear = -1;
+    }
     
+}
+
+int main(){
+
 }
